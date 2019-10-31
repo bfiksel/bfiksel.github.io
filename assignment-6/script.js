43f84f0111604906458a3fcc4108edd61448cec1
bfiksel <brandonfiksel@gmail.com>
2019-10-30 22:43:43 -0400

    6B code, including reflection

--- assignment-6/script.js
+++ assignment-6/script.js
@@ -46,6 +46,9 @@ function selectcolormh() {
         //image update
         document.getElementById("productimg").src = "./images/couch-img-2.png";
 
+        //currentpillow update for cart
+        currentpillow[0] = "mh";
+
 
     }
 
@@ -96,6 +99,9 @@ function selectcolorafs() {
 
         //image update
         document.getElementById("productimg").src = "./images/couch-img-1.png";
+
+        //currentpillow update for cart
+        currentpillow[0] = "afs";
     }
 
     function selectcolorrd() {
@@ -142,6 +148,9 @@ function selectcolorafs() {
         }
         //image update
         document.getElementById("productimg").src = "./images/couch-img-4.png";
+
+        //currentpillow update for cart
+        currentpillow[0] = "rd";
     }
 
     function selectcolorcd() {
@@ -192,6 +201,9 @@ function selectcolorafs() {
 
         //imageupdate
         document.getElementById("productimg").src = "./images/couch-img-3.png";
+
+        //currentpillow update for cart
+        currentpillow[0] = "cd";
     }
 
     function selectfillinghpb() {
@@ -224,6 +236,9 @@ function selectcolorafs() {
         {
             document.getElementById("mffilling").src = "./images/mf.png";
         }
+
+        //currentpillow update for cart
+        currentpillow[1] = "hpb";
     }
 
     function selectfillingmf() {
@@ -256,6 +271,9 @@ function selectcolorafs() {
         {
             document.getElementById("ddfilling").src = "./images/dd.png";
         }
+
+        //currentpillow update for cart
+        currentpillow[1] = "mf";
     }
 
 
@@ -289,14 +307,239 @@ function selectcolorafs() {
         {
             document.getElementById("mffilling").src = "./images/mf.png";
         }
+
+        //currentpillow update for cart
+        currentpillow[1] = "dd";
     }
 
+var cartlist;
+var cartlist = [,];
+
+
     function addtocart() {
+        console.log("cart list is")
+        console.log(cartlist)
         var x = document.getElementById("cartnumber");
         y=parseInt(x.innerText);
-        y+=1;
-        x.innerText = y;
-        console.log(y);
-        window.scrollTo({ top: 0, behavior: 'smooth' })
+        z=y+1;
+        x.innerText = z;
+        console.log(z);
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        localStorage.setItem("cartnum", z);
+        console.log("cart number is");
+        console.log(localStorage.getItem("cartnum"));
+
+        //adding current selection to cart list
+        //var cartpillow;
+        //var cartpillow = currentpillow;
+        //cartlist[y]=cartpillow;
+        cartlist[z]=JSON.parse(JSON.stringify(currentpillow))
+        console.log(z);
+        console.log(z-1);
+        console.log(cartlist[(z-1)]);
+        console.log(cartlist[z]);
+        localStorage.setItem("storedlist", JSON.stringify(cartlist));
     }
+
+
+
+    function custompillow() {
+        var couch = [
+        "afs",
+        "dd"
+         ];
+    }
+
+
+    //function pillowtocart() {
+        //if couch =["afs", "dd"]
+        //var img = document.createElement("img"); 
+        //img.src = "./images/couch-img-1.png";
+        //var src = document.getElementById("cartlist");
+        //src.appendChild(img);
+    //}
+
+
+//var cartdiv = document.getElementById('cartlist');
+ 
+//function pillowtocartold() {
+    //var img = new Image();
+    //img.src = './images/couch-img-1.png';
+    //pil1="img src="./images/couch-img-1.png""; 
+    //cartdiv.innerHTML += pil1;
+    //console.log(cartdiv);  
+    //console.log(cartdiv.innerHTML);
+//}
+
+function pillowtocart2() {
+    //set boolean true so that site will know at least 1 item was added
+        console.log("test");
+        console.log(localStorage.getItem("cartitem"));
+        //Boolean
+        localStorage.setItem("cartitem1boolean", "true");
+}
+
+function cartnumberupdate() {
+     //checkboolean
+    if (localStorage.getItem("cartitem1boolean") == "true") {
+    //console.log("cart number is");
+    var x = document.getElementById("cartnumber");
+    //console.log(x);
+    //console.log(x.innerHTML);
+    x.innerHTML = localStorage.getItem("cartnum");
+    //console.log("cart number is");
+    console.log(localStorage.getItem("cartnum"));
+}
+}
+
+
+var cartlist2;
+var cartlist2=[,];
+
+function cartupdate() {
+    //checkboolean
+    if (localStorage.getItem("cartitem1boolean") == "true") {
+    //document.getElementById("cartlistcart").src = localStorage.getItem("cartitem");
+    //console.log("added");
+    //}
+    //else 
+    //{
+     //document.getElementById("cartlistcart").src = " ";  
+    //}  
+    cartlist2=(JSON.parse(localStorage.getItem("storedlist")));
+    console.log("did it");
+    console.log(cartlist2);
+    localStorage.setItem("storedlist", JSON.stringify(cartlist2));}
+}
+
+function cartpageupdate(number) {
+    //cart afs dd
+    if ((cartlist2[number]!==undefined)&&(cartlist2[number]!==null)&&(cartlist2[number][0]=="afs")&&(cartlist2[number][1]=="dd")) { 
+        document.getElementById("cartitem"+(JSON.stringify(number))).src = "./images/afs-dd-cart.png";
+        document.getElementById("x"+(JSON.stringify(number))).src = "./images/x.png";
+    }
+    //cart afs hpb
+    if ((cartlist2[number]!==undefined)&&(cartlist2[number]!==null)&&(cartlist2[number][0]=="afs")&&(cartlist2[number][1]=="hpb")) { 
+        document.getElementById("cartitem"+(JSON.stringify(number))).src = "./images/afs-hpb-cart.png";
+        document.getElementById("x"+(JSON.stringify(number))).src = "./images/x.png";
+    }
+
+    //cart afs mf
+    if ((cartlist2[number]!==undefined)&&(cartlist2[number]!==null)&&(cartlist2[number][0]=="afs")&&(cartlist2[number][1]=="mf")) { 
+        document.getElementById("cartitem"+(JSON.stringify(number))).src = "./images/afs-mf-cart.png";
+        document.getElementById("x"+(JSON.stringify(number))).src = "./images/x.png";
+    }
+
+    //cart mh dd
+    if ((cartlist2[number]!==undefined)&&(cartlist2[number]!==null)&&(cartlist2[number][0]=="mh")&&(cartlist2[number][1]=="dd")) { 
+        document.getElementById("cartitem"+(JSON.stringify(number))).src = "./images/mh-dd-cart.png";
+        document.getElementById("x"+(JSON.stringify(number))).src = "./images/x.png";
+    }
+    //cart mh hpb
+    if ((cartlist2[number]!==undefined)&&(cartlist2[number]!==null)&&(cartlist2[number][0]=="mh")&&(cartlist2[number][1]=="hpb")) { 
+        document.getElementById("cartitem"+(JSON.stringify(number))).src = "./images/mh-hpb-cart.png";
+        document.getElementById("x"+(JSON.stringify(number))).src = "./images/x.png";
+    }
+
+    //cart mh mf
+    if ((cartlist2[number]!==undefined)&&(cartlist2[number]!==null)&&(cartlist2[number][0]=="mh")&&(cartlist2[number][1]=="mf")) { 
+        document.getElementById("cartitem"+(JSON.stringify(number))).src = "./images/mh-mf-cart.png";
+        document.getElementById("x"+(JSON.stringify(number))).src = "./images/x.png";
+    }
+
+    //cart cd dd
+    if ((cartlist2[number]!==undefined)&&(cartlist2[number]!==null)&&(cartlist2[number][0]=="cd")&&(cartlist2[number][1]=="dd")) { 
+        document.getElementById("cartitem"+(JSON.stringify(number))).src = "./images/cd-dd-cart.png";
+        document.getElementById("x"+(JSON.stringify(number))).src = "./images/x.png";
+    }
+    //cart cd hpb
+    if ((cartlist2[number]!==undefined)&&(cartlist2[number]!==null)&&(cartlist2[number][0]=="cd")&&(cartlist2[number][1]=="hpb")) { 
+        document.getElementById("cartitem"+(JSON.stringify(number))).src = "./images/cd-hpb-cart.png";
+        document.getElementById("x"+(JSON.stringify(number))).src = "./images/x.png";
+    }
+
+    //cart cd mf
+    if ((cartlist2[number]!==undefined)&&(cartlist2[number]!==null)&&(cartlist2[number][0]=="cd")&&(cartlist2[number][1]=="mf")) { 
+        document.getElementById("cartitem"+(JSON.stringify(number))).src = "./images/cd-mf-cart.png";
+        document.getElementById("x"+(JSON.stringify(number))).src = "./images/x.png";
+    }
+
+    //cart rd dd
+    if ((cartlist2[number]!==undefined)&&(cartlist2[number]!==null)&&(cartlist2[number][0]=="rd")&&(cartlist2[number][1]=="dd")) { 
+        document.getElementById("cartitem"+(JSON.stringify(number))).src = "./images/rd-dd-cart.png";
+        document.getElementById("x"+(JSON.stringify(number))).src = "./images/x.png";
+    }
+    //cart rd hpb
+    if ((cartlist2[number]!==undefined)&&(cartlist2[number]!==null)&&(cartlist2[number][0]=="rd")&&(cartlist2[number][1]=="hpb")) { 
+        document.getElementById("cartitem"+(JSON.stringify(number))).src = "./images/rd-hpb-cart.png";
+        document.getElementById("x"+(JSON.stringify(number))).src = "./images/x.png";
+    }
+
+    //cart rd mf
+    if ((cartlist2[number]!==undefined)&&(cartlist2[number]!==null)&&(cartlist2[number][0]=="rd")&&(cartlist2[number][1]=="mf")) { 
+        document.getElementById("cartitem"+(JSON.stringify(number))).src = "./images/rd-mf-cart.png";
+        document.getElementById("x"+(JSON.stringify(number))).src = "./images/x.png";
+    }
+
+
+}
+
+var cost;
+var cost = 0;
+//tried to create a function for total cost, realized there was an easier method
+//function totalcost(number) {
+    //if ((cartlist2[number]!==undefined)&&(cartlist2[number]!==null)) {
+    //cost = cost + 39.00;
+//}
+//}
+
+function calculatecost() {
+    var cartnumcost = localStorage.getItem("cartnum");
+    cost= 39.00*cartnumcost;
+    document.getElementById("costoncart").innerHTML=cost;
+}
+
+
+var currentpillow;
+var currentpillow=["afs", "dd"];
+
+function checkpillow() {
+    console.log("current pillow is");
+    console.log(currentpillow);
+}
+
+var cartlist3=[,];
+
+function removefromcart(number) {
+    //remove item image
+    document.getElementById("cartitem"+(JSON.stringify(number))).src = " ";
+    document.getElementById("cartitem"+(JSON.stringify(number))).height = 0;
+    document.getElementById("x"+(JSON.stringify(number))).src = " ";
+    document.getElementById("x"+(JSON.stringify(number))).height = 0;
+
+    //update cart number
+    var e = document.getElementById("cartnumber");
+    console.log(e);
+    console.log(e.innerHTML);
+    var f = (e.innerHTML)-1;
+    (e.innerHTML)=f;
+    localStorage.setItem("cartnum", f);
+
+    console.log("cart number is");
+    console.log(localStorage.getItem("cartnum"));
+
+   //update cartlist
+    //cartlist2=((JSON.parse(localStorage.getItem("storedlist"));
+    cartlist3=cartlist2.splice(number,1);
+    console.log("removed element from cart");
+    console.log(cartlist3);
+    localStorage.setItem("storedlist", JSON.stringify(cartlist3));
+    calculatecost();
+}
+
+var pillow1
+
+function setpillow1() {
+
+}
 
---
+++ assignment6legacy/assignment-6/script.js
@@ -0,0 +1,311 @@
+function selectcolormh() {
+        // mh selection
+        if (document.getElementById("mhcolor").src == "./images/mh.png") 
+        {
+            document.getElementById("mhcolor").src = "./images/mh-selected.png";
+        }
+        else 
+        {
+            document.getElementById("mhcolor").src = "./images/mh-selected.png";
+        }
+
+        //afs deselection
+
+         if (document.getElementById("afscolor").src == "./images/afs-selected.png") 
+        {
+            document.getElementById("afscolor").src = "./images/afs.png";
+        }
+        else 
+        {
+            document.getElementById("afscolor").src = "./images/afs.png";
+        }
+
+        //cd deselection
+
+         if (document.getElementById("cdcolor").src == "./images/cd-selected.png") 
+        {
+            document.getElementById("cdcolor").src = "./images/cd.png";
+        }
+        else 
+        {
+            document.getElementById("cdcolor").src = "./images/cd.png";
+        }
+
+
+        //rd deselection
+
+         if (document.getElementById("rdcolor").src == "./images/rd-selected.png") 
+        {
+            document.getElementById("rdcolor").src = "./images/rd.png";
+        }
+        else 
+        {
+            document.getElementById("rdcolor").src = "./images/rd.png";
+        }
+
+        //image update
+        document.getElementById("productimg").src = "./images/couch-img-2.png";
+
+
+    }
+
+
+function selectcolorafs() {
+        // afs selection
+        if (document.getElementById("afscolor").src == "./images/afs.png") 
+        {
+            document.getElementById("afscolor").src = "./images/afs-selected.png";
+        }
+        else 
+        {
+            document.getElementById("afscolor").src = "./images/afs-selected.png";
+        }
+
+
+        //mh deselection
+         if (document.getElementById("mhcolor").src == "./images/mh-selected.png") 
+        {
+            document.getElementById("mhcolor").src = "./images/mh.png";
+        }
+        else 
+        {
+            document.getElementById("mhcolor").src = "./images/mh.png";
+        }
+
+        //cd deselection
+
+         if (document.getElementById("cdcolor").src == "./images/cd-selected.png") 
+        {
+            document.getElementById("cdcolor").src = "./images/cd.png";
+        }
+        else 
+        {
+            document.getElementById("cdcolor").src = "./images/cd.png";
+        }
+
+        //rd deselection
+
+         if (document.getElementById("rdcolor").src == "./images/rd-selected.png") 
+        {
+            document.getElementById("rdcolor").src = "./images/rd.png";
+        }
+        else 
+        {
+            document.getElementById("rdcolor").src = "./images/rd.png";
+        }
+
+        //image update
+        document.getElementById("productimg").src = "./images/couch-img-1.png";
+    }
+
+    function selectcolorrd() {
+        //rd selection
+        if (document.getElementById("rdcolor").src == "./images/rd.png") 
+        {
+            document.getElementById("rdcolor").src = "./images/rd-selected.png";
+        }
+        else 
+        {
+            document.getElementById("rdcolor").src = "./images/rd-selected.png";
+        }
+
+        //cd deselection
+
+         if (document.getElementById("cdcolor").src == "./images/cd-selected.png") 
+        {
+            document.getElementById("cdcolor").src = "./images/cd.png";
+        }
+        else 
+        {
+            document.getElementById("cdcolor").src = "./images/cd.png";
+        }
+
+        //mh deselection
+         if (document.getElementById("mhcolor").src == "./images/mh-selected.png") 
+        {
+            document.getElementById("mhcolor").src = "./images/mh.png";
+        }
+        else 
+        {
+            document.getElementById("mhcolor").src = "./images/mh.png";
+        }
+
+        //afs deselection
+
+         if (document.getElementById("afscolor").src == "./images/afs-selected.png") 
+        {
+            document.getElementById("afscolor").src = "./images/afs.png";
+        }
+        else 
+        {
+            document.getElementById("afscolor").src = "./images/afs.png";
+        }
+        //image update
+        document.getElementById("productimg").src = "./images/couch-img-4.png";
+    }
+
+    function selectcolorcd() {
+        //cd selection
+        if (document.getElementById("cdcolor").src == "./images/cd.png") 
+        {
+            document.getElementById("cdcolor").src = "./images/cd-selected.png";
+        }
+        else 
+        {
+            document.getElementById("cdcolor").src = "./images/cd-selected.png";
+        }
+
+        //mh deselection
+         if (document.getElementById("mhcolor").src == "./images/mh-selected.png") 
+        {
+            document.getElementById("mhcolor").src = "./images/mh.png";
+        }
+        else 
+        {
+            document.getElementById("mhcolor").src = "./images/mh.png";
+        }
+
+
+        //rd deselection
+
+         if (document.getElementById("rdcolor").src == "./images/rd-selected.png") 
+        {
+            document.getElementById("rdcolor").src = "./images/rd.png";
+        }
+        else 
+        {
+            document.getElementById("rdcolor").src = "./images/rd.png";
+        }
+
+
+        //afs deselection
+
+         if (document.getElementById("afscolor").src == "./images/afs-selected.png") 
+        {
+            document.getElementById("afscolor").src = "./images/afs.png";
+        }
+        else 
+        {
+            document.getElementById("afscolor").src = "./images/afs.png";
+        }
+
+
+        //imageupdate
+        document.getElementById("productimg").src = "./images/couch-img-3.png";
+    }
+
+    function selectfillinghpb() {
+        //hpb selection
+        if (document.getElementById("hpbfilling").src == "./images/hpb.png") 
+        {
+            document.getElementById("hpbfilling").src = "./images/hpb-selected.png";
+        }
+        else 
+        {
+            document.getElementById("hpbfilling").src = "./images/hpb-selected.png";
+        }
+
+        //dd deselection
+        if (document.getElementById("ddfilling").src == "./images/dd-selected.png") 
+        {
+            document.getElementById("ddfilling").src = "./images/dd.png";
+        }
+        else 
+        {
+            document.getElementById("ddfilling").src = "./images/dd.png";
+        }
+
+        //mf deselection
+        if (document.getElementById("mffilling").src == "./images/mf-selected.png") 
+        {
+            document.getElementById("mffilling").src = "./images/mf.png";
+        }
+        else 
+        {
+            document.getElementById("mffilling").src = "./images/mf.png";
+        }
+    }
+
+    function selectfillingmf() {
+        //mf selection
+        if (document.getElementById("mffilling").src == "./images/mf.png") 
+        {
+            document.getElementById("mffilling").src = "./images/mf-selected.png";
+        }
+        else 
+        {
+            document.getElementById("mffilling").src = "./images/mf-selected.png";
+        }
+
+        //hpb deselection
+        if (document.getElementById("hpbfilling").src == "./images/hpb-selected.png") 
+        {
+            document.getElementById("hpbfilling").src = "./images/hpb.png";
+        }
+        else 
+        {
+            document.getElementById("hpbfilling").src = "./images/hpb.png";
+        }
+
+        //dd deselection
+        if (document.getElementById("ddfilling").src == "./images/dd-selected.png") 
+        {
+            document.getElementById("ddfilling").src = "./images/dd.png";
+        }
+        else 
+        {
+            document.getElementById("ddfilling").src = "./images/dd.png";
+        }
+    }
+
+
+    function selectfillingdd() {
+        //dd selection
+        if (document.getElementById("ddfilling").src == "./images/dd.png") 
+        {
+            document.getElementById("ddfilling").src = "./images/dd-selected.png";
+        }
+        else 
+        {
+            document.getElementById("ddfilling").src = "./images/dd-selected.png";
+        }
+
+        //hpb deselection
+        if (document.getElementById("hpbfilling").src == "./images/hpb-selected.png") 
+        {
+            document.getElementById("hpbfilling").src = "./images/hpb.png";
+        }
+        else 
+        {
+            document.getElementById("hpbfilling").src = "./images/hpb.png";
+        }
+
+        //mf deselection
+        if (document.getElementById("mffilling").src == "./images/mf-selected.png") 
+        {
+            document.getElementById("mffilling").src = "./images/mf.png";
+        }
+        else 
+        {
+            document.getElementById("mffilling").src = "./images/mf.png";
+        }
+    }
+
+    function addtocart() {
+        var x = document.getElementById("cartnumber");
+        y=parseInt(x.innerText);
+        y+=1;
+        x.innerText = y;
+        console.log(y);
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
+
+
+    function custompillow() {
+        var couch = [
+        "afs",
+        "dd"
+         ];
+
+
+    }
